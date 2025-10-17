6cfe59bcf6261ab95781fd4c5fe97d93cc65a330
eriksetiawan01 <149347297+eriksetiawan01@users.noreply.github.com>
2025-10-17 13:46:45 +0700

    upload tugas 4 laravel:read dan create data tabel genre dan author

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Author;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthorController extends Controller
 {
@@ -15,5 +16,41 @@ class AuthorController extends Controller
             "message" => "Get all resources",
             "data" => $authors
         ], 200);
+    }
+
+    public function store(Request $request)
+    {
+        //1. validator
+        $validator = Validator::make(request()->all(), [
+            'name' => 'required|string|max:255',
+            'bio' => 'required|string',
+            'cover_photo' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+        //2. check validaror error
+        if ($validator->fails()) {
+            return response()->json([
+                "success" => false,
+                "message" => "Validation Error.",
+                "errors" => $validator->errors()
+            ], 422);
+        }
+
+        //3. upload image
+        $image = $request->file('cover_photo');
+        $image->store('books','public');
+
+        //4. insert data
+        $authors = Author::create([
+            'name' => $request->name,
+            'bio' => $request->bio,
+            'cover_photo' => $image->hashName(),
+        ]);
+
+        //5. response
+        return response()->json([
+            "success" => true,
+            "message" => "Genres created successfully",
+            "data" => $authors
+        ], 201);
     }
 }
--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Genre;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class GenreController extends Controller
 {
@@ -11,10 +12,48 @@ class GenreController extends Controller
     {
 
         $genres = Genre::all();
+
+        if ($genres->isEmpty()) {
+            return response()->json([
+                "success" => true,
+                "message" => "Resources data not found",
+            ], 200);
+        }
+
         return response()->json([
             "success" => true,
             "message" => "Get all resources",
             "data" => $genres
         ], 200);
+    }
+
+    public function store(Request $request)
+    {
+        //1. validator
+        $validator = Validator::make(request()->all(), [
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+        ]);
+        //2. check validaror error
+        if ($validator->fails()) {
+            return response()->json([
+                "success" => false,
+                "message" => "Validation Error.",
+                "errors" => $validator->errors()
+            ], 422);
+        }
+
+        //4. insert data
+        $genres = Genre::create([
+            'name' => $request->name,
+            'description' => $request->description,
+        ]);
+
+        //5. response
+        return response()->json([
+            "success" => true,
+            "message" => "Genres created successfully",
+            "data" => $genres
+        ], 201);
     }
 }
